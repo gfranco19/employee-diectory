28c57debd4ff571f47e7ea74acdddab7d4f6de46
Giovanny Franco <gfranco19@live.com>
2020-12-26 22:05:05 -0800

    trying to push to github

---
+++ src/FilterInput.js
@@ -0,0 +1,24 @@
+  
+import React, { useEffect, useState } from "react";
+
+const FilterInput = ({ employees, updateEmployees }) => {
+  const [inputValue, updateInput] = useState("");
+
+  useEffect(() => {
+    const filteredEmployees =
+      inputValue === ""
+        ? employees
+        : employees.filter(
+            ({ name: { first } }) =>
+              first.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
+          );
+
+    updateEmployees(filteredEmployees);
+  }, [inputValue, employees]);
+
+  return (
+    <input value={inputValue} onChange={e => updateInput(e.target.value)} />
+  );
+};
+
+export default FilterInput;
